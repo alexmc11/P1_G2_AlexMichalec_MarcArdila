83a3495371e1e3d539982d8f5b973a37e8110a9f
alexm <alexm@MSI>
2017-03-21 18:40:05 +0100

    modify and startmap

--- P1_G2_AlexMichalec_MarcArdila/P1_G2_AlexMichalec_MarcArdila/Map.cpp
+++ P1_G2_AlexMichalec_MarcArdila/P1_G2_AlexMichalec_MarcArdila/Map.cpp
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 
-Map::Map() : md(new int*[NUM_ROWS])
+Map::Map() : md(new char*[NUM_ROWS])
 {
 	for (int i = 0; i < NUM_ROWS; i++) {
 
-		md[i] = new int[NUM_COLUMNS];
+		md[i] = new char[NUM_COLUMNS];
 	}
 
 	
@@ -19,11 +19,20 @@ Map::~Map()
 
 void Map::startMap()
 {
+	for (int i = 0; i < NUM_ROWS; i++) {
+		for (int j = 0; j < NUM_COLUMNS; j++)
+		{
+			md[i][j] = '.';
+		}
+	}
 }
 
-void Map::Modify()
+void Map::Modify(int column, int row, char newc)
 {
+	md[row][column] = newc;
 }
+
+
 
 void Map::printMap()
 {
